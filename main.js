6e8c5a67935bd6f3a1b2a22f5e734bf3fb0e0d27
gianlucapinna99 <gianlucapinna99@gmail.com>
2023-02-28 17:29:20 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const shoppingList [
+    "pane",
+    "vino",
+    "latte",
+    "patate",
+    "gorgonzola",
+    "miele"
+];
+
+const ul = document.getElementById("shopping-list");
+
+
+while (i < 6){
+    console.log(shoppingList[i])
+}
+
